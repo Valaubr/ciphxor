95e90769ae3e4a45f12546e738faf91c37863917
unknown <stalker039906@gmail.com>
2020-04-21 16:25:31 +0300

    перезалив в отдельный реп.

---
+++ src/main/java/Main.java
@@ -0,0 +1,97 @@
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import java.io.*;
+
+/**
+ *
+ * @author valaubr
+ */
+public class Main {
+
+    @Option(name = "-c", forbids = {"-d"}, metaVar = "encryption", usage = "Encryption key")
+    private String encrypt;
+
+    @Option(name = "-d", forbids = {"-c"}, metaVar = "decryption", usage = "Decryption key")
+    private String decrypt;
+
+    @Argument(index = 0, required = true, metaVar = "InputName", usage = "Input file name")
+    private String inputFileName;
+
+    @Option(name = "-o", required = false, metaVar = "OutputName", usage = "Output file name")
+    private String outputFileName;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Main().init(args);
+    }
+
+    private void init(String[] args) {
+        CmdLineParser parser = new CmdLineParser(this);
+
+        try {
+            parser.parseArgument(args);
+            this.run();
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            parser.printUsage(System.err);
+        }
+    }
+
+    private void run() {
+        //Кастыль из за условия с encrypt - decrypt в разные опции...
+        if (encrypt == null){
+            encrypt = decrypt;
+        }
+
+        encrypt = encrypt.replaceAll("(-c|-d)", "").strip();
+
+        Integer key = Integer.parseInt(encrypt, 16);
+
+        if (Integer.toHexString(key).equals(encrypt)){
+            System.out.println("yep");
+        } else {
+            System.out.println("Nope");
+        }
+        try {
+            encryptDecrypt(key);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void encryptDecrypt(Integer key) throws IOException {
+        File incFile = new File(inputFileName);
+        if (outputFileName == null){
+            outputFileName = incFile.getName();
+        }
+        outputFileName = outputFileName.replaceFirst("-o", "").strip();
+        File outFile = new File(outputFileName);
+        // Сразу скажу, почему так: будет несколько проблематично хранить
+        // в ОЗУ текстовый файл и разбивать его по символам (Да да, первый сем меня этому научил :D)
+        // К сожалению, с тем что я испольхую варианты только "Убрать всё и написать вот это"
+        // (в случае если мы весь тексn зашифровали в оперативе и всё выгружаем назад)
+        // либо дописывать в конец (Будет наглядное пособие как создать
+        // файл огромных размеров с помощью алгоритма шифрования, не защитив при этом файл :D)
+        boolean check = outFile.createNewFile();
+
+        FileInputStream isr = new FileInputStream(incFile.getAbsolutePath());
+        FileOutputStream osr = new FileOutputStream(outFile.getPath());
+        if (!check){
+            System.out.println("Файл не возможно переписать");
+            System.exit(-1);
+        }
+
+        while (isr.available() > 0){
+            char oneChar = (char) isr.read();
+            osr.write(oneChar ^ key);
+        }
+        isr.close();
+        osr.close();
+        System.out.println("Good");
+    }
+}
---
+++ src/test/java/Tests.java
@@ -0,0 +1,73 @@
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Tests {
+
+    private Path testFileDecrypted = Paths.get("src","test", "resources", "testFileDecrypted.txt");
+    private Path testFileEncrypted = Paths.get("src","test", "resources", "testFileEncrypted.txt");
+    private String fileName = "testFileEncrypted.txt";
+    private File fileD = new File(testFileDecrypted.toString());
+    private File fileE = new File(testFileEncrypted.toString());
+
+    @Test
+    void encryptionText() throws IOException {
+        Main.main(new String[]{"-c 1a3b3f5c", testFileDecrypted.toString(), "-o " + fileName });
+        File f = new File(fileName);
+        assertTrue(FileUtils.contentEquals(f, fileE));
+        assertFalse(FileUtils.contentEquals(f, fileD));
+    }
+
+    @Test
+    void decryptionText() throws IOException {
+        Main.main(new String[]{"-c 1a3b3f5c", testFileEncrypted.toString(), "-o " + fileName });
+        File f = new File(fileName);
+        assertTrue(FileUtils.contentEquals(f, fileD));
+        assertFalse(FileUtils.contentEquals(f, fileE));
+    }
+
+    @Test
+    void noNewNameOption() {
+        Main.main(new String[]{"-c 1a3b3f5c", testFileEncrypted.toString() });
+        assertTrue(new File("testFileEncrypted.txt").exists());
+    }
+
+    @Test
+    void badKey() {
+        assertThrows(NumberFormatException.class, () -> {
+            Main.main(new String[]{"-c 13p5", testFileDecrypted.toString()});
+        });
+        assertThrows(NumberFormatException.class, () -> {
+            Main.main(new String[]{"-c 13fdsa5", testFileDecrypted.toString()});
+        });
+        assertThrows(NumberFormatException.class, () -> {
+            Main.main(new String[]{"-c 1d3a5ffq", testFileDecrypted.toString()});
+        });
+        assertThrows(NumberFormatException.class, () -> {
+            Main.main(new String[]{"-c f0eeee25g5f", testFileDecrypted.toString()});
+        });
+    }
+
+    @Test
+    void twoOptions() {
+        Main.main(new String[]{"-c f0eeee25g5f", "-d 12345", testFileDecrypted.toString()});
+    }
+
+    @AfterEach
+    void deleteFinalFile(){
+        File file = new File(fileName);
+        file.delete();
+        file = new File("testFileDecrypted.txt");
+        file.delete();
+        file = new File("testFileEncrypted.txt");
+        file.delete();
+    }
+}
